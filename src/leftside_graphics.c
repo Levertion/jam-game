f07a4ec5d747cc9688a5b73f5f9030e5fb1d7326
Martin Tsvetkov <marti@tsvetkov.eu.com>
2020-05-01 16:38:49 +0300

    Added (broken) conveyor belt

--- src/leftside_graphics.c
+++ src/leftside_graphics.c
@@ -1,5 +1,18 @@
 #include "leftside_graphics.h"
+#include "leftside_logic.h"
 #include "raylib.h"
-void draw_leftside(){
-    ClearBackground(BLUE);
+#include "shapes.h"
+extern Shape AllShapes[3];
+void draw_leftside()
+{
+    ClearBackground(GRAY);
+
+    for (int i = 0; i < items_conveyor.length; i++)
+    {
+        if (RING_INDEX_POS(items_conveyor, i).y >= 0)
+        {
+            DrawTexture(AllShapes[RING_INDEX_IDS(items_conveyor, i)].art,
+                        RING_INDEX_POS(items_conveyor, i).x, RING_INDEX_POS(items_conveyor, i).y, WHITE);
+        }
+    }
 }
